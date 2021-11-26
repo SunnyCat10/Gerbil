1c5679200904fb065f93b5bc5a0a08410994ca6e
SunnyCat10 <daniel.dovgun@gmail.com>
2021-11-26 23:22:18 +0200

    Added interfaces for behavior and shot node.

---
+++ BehaviourTree/Interfaces/IActor.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace Gerbil.BehaviourTree.Interfaces
+{
+    /// <summary>
+    /// Base interface for all the actor that implement the behavior tree.
+    /// </summary>
+    interface IActor
+    {
+        public void Shoot(Vector2 shootingDirection);
+    }
+}
