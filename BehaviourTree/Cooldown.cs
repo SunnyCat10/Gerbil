8fd5348e463c64aaa55b2962240ea77104e1a009
SunnyCat10 <daniel.dovgun@gmail.com>
2021-11-26 21:54:53 +0200

    Updated cooldown node and started working on repeat node.

--- BehaviourTree/Cooldown.cs
+++ BehaviourTree/Cooldown.cs
@@ -2,20 +2,37 @@ using Godot;
 using Gerbil.BehaviourTree;
 using Godot.Collections;
 
+/// <summary>
+/// Cooldown node for creating delay between actions.
+/// </summary>
 public class Cooldown : BaseNode
 {
 	[Export]
 	float duration = 3f;
 
 	bool isInCooldown = false;
+	BaseNode child;
+
+	public override void _Ready()
+	{
+		child = (BaseNode)GetChild(0);
+	}
 
 	public override State Tick(Node2D actor, Dictionary blackboard)
 	{
 		if (isInCooldown)
+		{
 			return State.Running;
-		GetTree().CreateTimer(duration).Connect("timeout", this, nameof(Timeout));
-		isInCooldown = true;
-		return State.Succeeded;
+		}
+		State childState = child.Tick(actor, blackboard);
+		if (childState == State.Failed)
+			return State.Failed;
+		else
+		{
+			GetTree().CreateTimer(duration).Connect("timeout", this, nameof(Timeout));
+			isInCooldown = true;
+			return State.Succeeded;
+		}	
 	}
 
 	public void Timeout()
---
+++ BehaviourTree/Repeat.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+using Gerbil.BehaviourTree;
+using Godot.Collections;
+
+public class Repeat : BaseNode
+{
+	[Export]
+	private int amount = 3;
+
+	private int counter = 0;
+	private bool isRepeating = false;
+	private BaseNode child;
+
+	public override void _Ready()
+	{	
+		isRepeating = true;
+		child = (BaseNode)GetChild(0);
+	}
+
+	public override State Tick(Node2D actor, Dictionary blackboard)
+	{
+		return TickChild(actor, blackboard);
+	}
+
+	private State TickChild(Node2D actor, Dictionary blackboard)
+	{
+		State childState;
+		childState = child.Tick(actor, blackboard);
+		switch (childState)
+		{
+			case State.Running:
+				return State.Running;
+			case State.Failed:
+				counter = 0;
+				return State.Failed;
+			case State.Succeeded:
+				++counter;
+				if (counter >= amount)
+				{
+					counter = 0;
+					return State.Succeeded;
+				}
+				return State.Running;
+		}
+		return State.Failed;
+	}
+}
