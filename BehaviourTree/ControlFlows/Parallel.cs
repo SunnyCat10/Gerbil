ba0654d09d82ee045feadb31c47c2994042e8e3b
SunnyCat10 <daniel.dovgun@gmail.com>
2021-12-28 20:43:08 +0200

    Added  all succeeded policy to parallel control flow node.

--- BehaviourTree/ControlFlows/Parallel.cs
+++ BehaviourTree/ControlFlows/Parallel.cs
@@ -23,20 +23,21 @@ namespace Gerbil.BehaviourTree.ControlFlows
 		{
 			children = GetChildren();
 			results = new State[children.Count];
-            switch (policy)
-            {
-                case Policy.OneSucceeded:
-                    executionPolicy = OneSucceededPolicy;
-					break;
-                case Policy.AllSucceeded:
-                    break;
-                case Policy.AllFinishedRunning:
-                    break;
-                default:
+			switch (policy)
+			{
+				case Policy.OneSucceeded:
 					executionPolicy = OneSucceededPolicy;
 					break;
-            }
-        }
+				case Policy.AllSucceeded:
+					executionPolicy = AllSucceededPolicy;
+					break;
+				case Policy.AllFinishedRunning:
+					break;
+				default:
+					executionPolicy = OneSucceededPolicy;
+					break;
+			}
+		}
 
 		public override State Tick(Node2D actor, Dictionary blackboard)
 		{
@@ -45,35 +46,76 @@ namespace Gerbil.BehaviourTree.ControlFlows
 
 		//TODO: Implement
 		private State OneSucceededPolicy(Node2D actor, Dictionary blackboard)
-        {
+		{
 			GD.Print("Not Implemented Yet");
-			for (int i = 0; i < children.Count; i++)
-			{
-				if (results[i] == State.Running)
-					results[i] = ((BaseNode)children[i]).Tick(actor, blackboard);
+			//for (int i = 0; i < children.Count; i++)
+			//{
+			//	if (results[i] == State.Running)
+			//		results[i] = ((BaseNode)children[i]).Tick(actor, blackboard);
 
-				if (results[i] == State.Succeeded)
-                {
-					for (int j = 0; j < children.Count; j++) // Resets all the states of the children.
-						children[j] = State.Running;
-					return State.Succeeded;
-                }
+			//	if (results[i] == State.Succeeded)
+			//	{
+			//		for (int j = 0; j < children.Count; j++) // Resets all the states of the children.
+			//			children[j] = State.Running;
+			//		return State.Succeeded;
+			//	}
+			//}
+			//return State.Failed;
+
+			return State.Failed;
+		}
+
+		//TODO: TESTSS
+		private State AllSucceededPolicy(Node2D actor, Dictionary blackboard)
+		{
+			GD.Print("Not Implemented Yet");
+			int succeededChildren = 0;
+            for (int i = 0; i < children.Count; i++)
+            {
+				if (results[i] == State.Running)
+				{
+					State result = ((BaseNode)children[i]).Tick(actor, blackboard);
+                    switch (result)
+                    {
+                        case State.Running:
+                            continue;
+                        case State.Failed:
+							succeededChildren = 0;
+							ResetResults();
+							return State.Failed;
+                        case State.Succeeded:
+							++succeededChildren; 
+							if (succeededChildren == children.Count)
+                            {
+								succeededChildren = 0;
+								ResetResults();
+								return State.Succeeded;
+                            }
+							else
+                            {
+								results[i] = State.Succeeded;
+								break;
+							}				
+                    }
+                }		
 			}
 			return State.Failed;
 		}
 
 		//TODO: Implement
-		private State AllSucceededPolicy(Node2D actor, Dictionary blackboard)
-        {
-			GD.Print("Not Implemented Yet");
-			return State.Failed;
-        }
-
-		//TODO: Implement
 		private State AllFinishedRunningPolicy(Node2D actor, Dictionary blackboard)
-        {
+		{
 			GD.Print("Not Implemented Yet");
 			return State.Failed;
 		}
+
+		/// <summary>
+		/// Reset the results array with the default value - State.Running .
+		/// </summary>
+		private void ResetResults()
+        {
+            for (int i = 0; i < results.Length; i++)
+				results[i] = State.Running;
+        }
 	}
 }
